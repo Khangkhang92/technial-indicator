7cb3605a43f9f7b291e021cbcf1aee246c082b03
Khangkhang92 <khang.luongvan18@gmail.com>
2022-10-25 12:29:52 +0700

    saving just lates 60 results per ticker

--- main.py
+++ main.py
@@ -16,30 +16,29 @@ def indicators_caculate(rough_data_by_ticker, session):
     for day in close_price_d1:
         close.append(day["close"])
     close_series = pd.Series(close)
-
     rsi = ta.rsi(close_series).fillna(0).tolist()
-    sma7 = ta.ma("sma", close_series, length=7).fillna(0).tolist()
-    sma60 = ta.ma("sma", close_series, length=60).fillna(0).tolist()
+    sma9 = ta.ma("sma", close_series, length=9).fillna(0).tolist()
+    sma20 = ta.ma("sma", close_series, length=20).fillna(0).tolist()
     macd = ta.macd(close_series).fillna(0)
     bbands = ta.bbands(close_series).fillna(0)
-
+    start = len(rsi) - 60
     for index, rsi in enumerate(rsi):
-        sma = {}
-        sma["sma7"] = sma7[index]
-        sma["sma60"] = sma60[index]
-        indicator_result = IndicatorsResults(
-            stock_id=rough_data_by_ticker.stock_id,
-            date=close_price_d1[index]["date"],
-            rsi=rsi,
-            sma=sma,
-            macd=macd.loc[index].to_dict(),
-            bbands=bbands.loc[index].to_dict(),
-        )
-        indicator_results.append(indicator_result)
-        print(index)
-    session.bulk_save_objects(indicator_results)
-    session.commit()
+        if index >= start:
+            sma = {}
+            sma["sma9"] = sma9[index]
+            sma["sma20"] = sma20[index]
+            indicator_result = IndicatorsResults(
+                stock_id=rough_data_by_ticker.stock_id,
+                date=close_price_d1[index]["date"],
+                rsi=rsi,
+                sma=sma,
+                macd=macd.loc[index].to_dict(),
+                bbands=bbands.loc[index].to_dict(),
+            )
+            indicator_results.append(indicator_result)
+
     print(f"caculate for {rough_data_by_ticker.stock_id} is ok")
+    return indicator_results
 
 
 @app.get("/")
@@ -58,8 +57,12 @@ async def root(session: Session = Depends(ScopedSession)):
     """
 
     stockprice_alldata = session.execute(stmt).fetchall()
-
+    results = []
     for rough_data_by_ticker in stockprice_alldata:
         if len(rough_data_by_ticker.d1) >= 60:
-            indicators_caculate(rough_data_by_ticker, session)
+            result = indicators_caculate(rough_data_by_ticker, session)
+            results.extend(result)
+    print("saving indicator results")
+    session.bulk_save_objects(results)
+    session.commit()
     return {"msg": "sucessful saving data"}
